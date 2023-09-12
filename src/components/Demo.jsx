d67d044837bad8f112a9732a8f7d3c812dd8d6be
Piorek1994 <pprus94@gmail.com>
2023-09-12 21:10:07 +0200

    demo component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+          />
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
