22df2eefc257db5f835495667f3f72406091abbe
Piorek1994 <pprus94@gmail.com>
2023-09-12 20:51:25 +0200

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Hero from "./components/Hero";
 import Demo from "./components/Demo";
 
+import "./App.css";
+
 const App = () => {
-  return <div>Affpp</div>;
+  return (
+    <div>
+      <main>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <div className="app">
+          <Hero />
+          <Demo />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default App;
